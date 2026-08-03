f65fa8640b8722a1dda791a51436f1189bf33bc8
Mahima <mmalviya89808@gmail.com>
2026-08-03 14:19:01 +0530

    Read API_BASE_URL from environment variable with localhost fallback

--- utils/api_client.py
+++ utils/api_client.py
@@ -24,7 +24,9 @@ import requests
 # Configuration
 # --------------------------------------------------
 
-API_BASE_URL = "http://127.0.0.1:8000"
+import os
+
+API_BASE_URL = os.environ.get("API_BASE_URL", "http://127.0.0.1:8000")
 
 DEFAULT_TIMEOUT = 30
 
